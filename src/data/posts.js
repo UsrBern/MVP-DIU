722b31d62ec4b6cb09f5a821517680edcf72da2c
UsrBern <73857532+UsrBern@users.noreply.github.com>
2024-10-31 21:56:33 -0300

    Update posts.js

--- src/data/posts.js
+++ src/data/posts.js
@@ -1,1 +1,28 @@
+// src/data/posts.js
+export const posts = [
+  {
+    id: 1,
+    title: "Corte de Electricidad",
+    description: "Un corte de electricidad ha afectado varias cuadras del centro. No se reportan heridos graves.",
+    gravity: "media",
+    location: "Región Metropolitana",
+    type: "Corte de Electricidad",
+  },
+  {
+    id: 2,
+    title: "Incendio",
+    description: "Los residentes de la calle Maple afectados por un incendio necesitan suministros y refugio temporal.",
+    gravity: "grave",
+    location: "Región Metropolitana",
+    type: "Incendio en Casa",
+  },
+  {
+    id: 3,
+    title: "Tsunami",
+    description: "Los residentes de la zona costera necesitan asistencia urgente después de que un tsunami devastara sus hogares.",
+    gravity: "muy grave",
+    location: "Región de Valparaíso",
+    type: "Tsunami",
+  },
+];
 

cfcaf4a602d85d96c2961011cb9e558a6236b906
UsrBern <73857532+UsrBern@users.noreply.github.com>
2024-10-31 22:02:23 -0300

    Update forum.jsx

--- src/pages/forum.jsx
+++ src/pages/forum.jsx
@@ -1,1 +1,26 @@
+import React, { useState } from 'react';
+import Filter from '../components/Filter';
+import Post from '../components/Post';
+import { posts } from '../data/posts';
+
+const Forum = () => {
+  const [filteredPosts, setFilteredPosts] = useState(posts);
+
+  const handleFilterChange = (e) => {
+    const { name, value } = e.target;
+    const filtered = posts.filter(post => post[name] === value || value === "");
+    setFilteredPosts(filtered);
+  };
+
+  return (
+    <div>
+      <Filter onFilterChange={handleFilterChange} />
+      <div className="posts-list">
+        {filteredPosts.map(post => <Post key={post.id} post={post} />)}
+      </div>
+    </div>
+  );
+};
+
+export default Forum;
 
